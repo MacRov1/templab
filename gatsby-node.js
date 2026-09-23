b5bf7e1ff463a9c24705cf7693ee291c632b0983
imperiodevuy <imperio.dev.uy@gmail.com>
2026-09-23 19:04:36 -0300

    Corrige dependencias y entorno de deploy

--- gatsby-node.js
+++ gatsby-node.js
@@ -39,16 +39,3 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     })
   })
 }
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-
-  if (node.internal.type === `Mdx`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
-}
